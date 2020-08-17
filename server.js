9492f5bc5e5c92add8983339db81f916d239750b
quangdang87 <vinhquangdang1@gmail.com>
2020-08-17 07:24:27 -0700

    Added MongoDB to the app

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
+//connect Database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => res.json({msg: "Welcome to Contact Keeper API"}));

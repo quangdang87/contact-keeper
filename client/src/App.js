0b6c9f9492c6a6d1e899a54c6439c5080e5d3260
quangdang87 <vinhquangdang1@gmail.com>
2020-09-01 10:00:27 -0700

    finished added contact to UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
+import Navbar from "./components/layout/Navbar";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
+import ContactState from "./context/contact/contactState";
 import "./App.css";
 
-function App() {
-  return <div>Hello</div>;
-}
+export const App = () => {
+  return (
+    <ContactState>
+      <Router>
+        <>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </>
+      </Router>
+    </ContactState>
+  );
+};
 
 export default App;
